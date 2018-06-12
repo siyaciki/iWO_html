f9431a32a31e0a8a059d98fc3f7a52c965bb5f84
siyacikisikagaya <2900862178@qq.com>
2018-06-12 18:16:33 +0800

    ch

---
+++ js/localstroge.js
@@ -0,0 +1,13 @@
+var username = localStorage.getItem('username');
+var login_time = localStorage.getItem('login_time');
+var token = localStorage.getItem('token');
+var server_addr = localStorage.getItem('server_addr');
+var mac = localStorage.getItem('mac');
+var flight_id=localStorage.getItem('flight_id');
+var user_info = {
+	'username': username,
+	'login_time': login_time,
+	'token': token,
+	'mac': mac,
+	'flight_id':flight_id
+};
