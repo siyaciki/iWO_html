9240492eeacd252d5c0ee7938bcd80e89cd1c31c
siyacikisikagaya <2900862178@qq.com>
2018-06-15 17:28:45 +0800

    ch

--- js/localstroge.js
+++ js/localstroge.js
@@ -4,6 +4,7 @@ var token = localStorage.getItem('token');
 var server_addr = localStorage.getItem('server_addr');
 var mac = localStorage.getItem('mac');
 var flight_id=localStorage.getItem('flight_id');
+var crew_id=localStorage.getItem('crew_id');
 var user_info = {
 	'username': username,
 	'login_time': login_time,
--- unpackage/release/H51FBF0E3_0604124134/assets/apps/H51FBF0E3/www/README.md
+++
@@ -1,3 +0,0 @@
-#app应用界面
-<br/>作者：siyaciki<br/>
-界面使用：mui,bootstrap<br/>
